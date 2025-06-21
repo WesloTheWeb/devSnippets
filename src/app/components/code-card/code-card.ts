8d2f7647d08471a9fb6fe5fbd6eda0ca9c655ea8
Wesley <wesleyadwebster@gmail.com>
2025-06-21 05:07:18 -0500

    small changes to similarity marker colors

--- src/app/components/code-card/code-card.ts
+++ src/app/components/code-card/code-card.ts
@@ -13,6 +13,10 @@ export class CodeCard {
   @Input() snippet: Snippet | null = null;
   copied = false;
 
+  HIGH_SIMILARITY = 0.45;
+  MEDIUM_SIMILARITY = 0.35;
+  LOW_SIMILARITY = 0.15;
+
   copyCode() {
     if (this.snippet?.code) {
       navigator.clipboard.writeText(this.snippet.code).then(() => {
