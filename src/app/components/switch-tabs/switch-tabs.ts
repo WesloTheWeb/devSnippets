c55e690655abac9a0a740fbc52dafd39480fa268
Wesley <wesleyadwebster@gmail.com>
2025-06-23 12:26:59 -0500

    switch tabs

--- src/app/components/switch-tabs/switch-tabs.ts
+++ src/app/components/switch-tabs/switch-tabs.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-switch-tabs',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './switch-tabs.html',
   styleUrl: './switch-tabs.scss'
 })
 export class SwitchTabs {
+  activeTab: 'search' | 'add' = 'search';
+  
+  @Output() tabChanged = new EventEmitter<'search' | 'add'>();
 
+  setActiveTab(tab: 'search' | 'add') {
+    this.activeTab = tab;
+    this.tabChanged.emit(tab); 
+  }
 }
